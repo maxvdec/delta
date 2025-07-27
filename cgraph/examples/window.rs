7cbc701485620988ceb3b0bc3f74f5e122bb8797
Max Van den Eynde <maxvdec@gmail.com>
2025-07-27 21:32:20 +0200

    Finished window event implementation

--- cgraph/examples/window.rs
+++ cgraph/examples/window.rs
@@ -1,6 +1,18 @@
-use cgraph::{self, app::Window};
+use cgraph::{
+    self,
+    app::{CoreEvent, CoreEventReference, CoreWindowEvent, Window},
+};
 
 fn main() {
-    let win = Window::new("Window", 800, 600);
+    let mut win = Window::new("Window", 800, 600);
+    win.on_event(CoreEventReference::WindowEvent, |_, event| match event {
+        CoreEvent::WindowEvent(event) => match event {
+            CoreWindowEvent::KeyboardInput(input) => {
+                println!("Key pressed: {:?}", input);
+            }
+            _ => (),
+        },
+        _ => (),
+    });
     win.launch();
 }
