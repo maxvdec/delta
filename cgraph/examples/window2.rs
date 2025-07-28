00f96200f42904fe169e94e4fca421944333575f
Max Van den Eynde <maxvdec@gmail.com>
2025-07-28 10:55:22 +0200

    Finished window decoration basic API

--- cgraph/examples/window2.rs
+++ cgraph/examples/window2.rs
@@ -4,7 +4,7 @@ use cgraph::{
 };
 
 fn main() {
-    let mut win = Window::new("Window", 800, 600, Some(WindowOptions::resizable()));
+    let mut win = Window::new("Window", 800, 600, Some(WindowOptions::no_titlebar()));
     win.on_event(CoreEventReference::WindowEvent, |_, event| {
         if let CoreEvent::WindowEvent(CoreWindowEvent::KeyboardInput(input)) = event {
             println!("Key pressed: {input:?}");
