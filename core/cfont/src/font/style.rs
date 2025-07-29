72ec18b432f4cf927db766d494eb30c41a51b9c7
Max Van den Eynde <maxvdec@gmail.com>
2025-07-29 14:30:50 +0200

    Documented Core Font for font weights

--- core/cfont/src/font/style.rs
+++ core/cfont/src/font/style.rs
@@ -1,29 +1,45 @@
 #[derive(Debug, Clone, PartialEq, Eq, Default)]
+/// Represents a text style with properties such as weight, italic, and underlined.
 pub struct TextStyle {
+    /// The font weight, represented as a `FontWeight`.
     pub weight: FontWeight,
+    /// Whether the text is italic.
     pub italic: bool,
+    /// Whether the text is underlined.
     pub underlined: bool,
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Default)]
+/// Represents the weight of a font.
 pub enum FontWeight {
     #[default]
+    /// Normal weight (400).
     Normal = 400,
+    /// Thin weight (100).
     Thin = 100,
+    /// Extra light weight (200)
     ExtraLight = 200,
+    /// Light weight (300).
     Light = 300,
+    /// Medium weight (500).
     Medium = 500,
+    /// Semi-bold weight (600).
     SemiBold = 600,
+    /// Bold weight (700).
     Bold = 700,
+    /// Extra bold weight (800).
     ExtraBold = 800,
+    /// Black weight (900).
     Black = 900,
 }
 
 impl TextStyle {
+    /// Creates a new `TextStyle` with default properties.
     pub fn new() -> Self {
         Self::default()
     }
 
+    /// Creates a `TextStyle` with bold weight.
     pub fn bold() -> Self {
         Self {
             weight: FontWeight::Bold,
@@ -32,6 +48,7 @@ impl TextStyle {
         }
     }
 
+    /// Creates a `TextStyle` with italic style.
     pub fn italic() -> Self {
         Self {
             weight: FontWeight::Normal,
@@ -40,6 +57,7 @@ impl TextStyle {
         }
     }
 
+    /// Creates a `TextStyle` with underlined style.
     pub fn underlined() -> Self {
         Self {
             weight: FontWeight::Normal,
@@ -48,21 +66,25 @@ impl TextStyle {
         }
     }
 
+    /// Creates a `TextStyle` with the specified weight, italic, and underlined properties.
     pub fn with_weight(mut self, weight: FontWeight) -> Self {
         self.weight = weight;
         self
     }
 
+    /// Sets the italic property of the text style.
     pub fn with_italic(mut self, italic: bool) -> Self {
         self.italic = italic;
         self
     }
 
+    /// Sets the underlined property of the text style.
     pub fn with_underlined(mut self, underlined: bool) -> Self {
         self.underlined = underlined;
         self
     }
 
+    /// Checks if the text style is bold.
     pub fn is_bold(&self) -> bool {
         matches!(
             self.weight,
